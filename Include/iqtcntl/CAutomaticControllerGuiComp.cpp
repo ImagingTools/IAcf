753c2b8073479fb464f0d2f258b4920b69f72377
wg <wg@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2008-10-15 15:42:42 +0000

    Library iqtcntl added. Automatic controller GUI added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@30 f04c80de-fe56-0410-a3ee-ab8cdec974cc

---
+++ Include/iqtcntl/CAutomaticControllerGuiComp.cpp
@@ -0,0 +1,38 @@
+#include "iqtcntl/CAutomaticControllerGuiComp.h"
+
+
+namespace iqtcntl
+{
+
+
+// reimplemented (imod::IModelEditor)
+
+void CAutomaticControllerGuiComp::UpdateModel() const
+{
+	icntl::IAutomaticController* modelPtr = GetObjectPtr();
+	if (IsGuiCreated() && (modelPtr != NULL)){
+		modelPtr->SetWorkMode(AutomaticButton->isChecked()? icntl::IAutomaticController::WM_AUTOMATIC: icntl::IAutomaticController::WM_MANUAL);
+	}
+}
+
+
+void CAutomaticControllerGuiComp::UpdateEditor()
+{
+	const icntl::IAutomaticController* modelPtr = GetObjectPtr();
+	if (IsGuiCreated() && (modelPtr != NULL)){
+		AutomaticButton->setChecked(modelPtr->GetWorkMode() == icntl::IAutomaticController::WM_AUTOMATIC);
+	}
+}
+
+
+// protected slots
+
+void CAutomaticControllerGuiComp::on_AutomaticButton_toggled(bool /*checked*/)
+{
+	UpdateModel();
+}
+
+
+} // namespace iqtcntl
+
+
---
+++ Include/iqtcntl/CAutomaticControllerGuiComp.h
@@ -0,0 +1,45 @@
+#ifndef iqtcntl_CAutomaticControllerGuiComp_included
+#define iqtcntl_CAutomaticControllerGuiComp_included
+
+
+// ACF includes
+#include "imod/IModel.h"
+#include "iqtgui/TDesignerGuiObserverCompBase.h"
+
+#include "icntl/IAutomaticController.h"
+
+#include "iqtcntl/iqtcntl.h"
+
+#include "iqtcntl/Generated/ui_CAutomaticControllerGuiComp.h"
+
+
+namespace iqtcntl
+{
+
+
+class CAutomaticControllerGuiComp:
+			public iqtgui::TDesignerGuiObserverCompBase<Ui::CAutomaticControllerGuiComp, icntl::IAutomaticController>
+{
+	Q_OBJECT
+
+public:
+	typedef iqtgui::TDesignerGuiObserverCompBase<Ui::CAutomaticControllerGuiComp, icntl::IAutomaticController> BaseClass;
+
+	I_BEGIN_COMPONENT(CAutomaticControllerGuiComp)
+	I_END_COMPONENT
+
+	// reimplemented (imod::IModelEditor)
+	virtual void UpdateModel() const;
+	virtual void UpdateEditor();
+
+protected slots:
+	void on_AutomaticButton_toggled(bool checked);
+};
+
+
+} // namespace iqtcntl
+
+
+#endif // !iqtcntl_CAutomaticControllerGuiComp_included
+
+
