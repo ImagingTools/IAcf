fcb88b30e4136d3c34f4207e1e9d37f05ec9e911
wg <wg@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2008-12-11 00:05:38 +0000

    Correction of XCODE project generating script (target type created, file name with prefix and extension based on target type). XCODE projects for Impl generated.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@60 f04c80de-fe56-0410-a3ee-ab8cdec974cc

---
+++ Impl/Xcd/ConvertImpl.bat
@@ -0,0 +1,2 @@
+cd %IACFDIR%/Impl
+%ACFDIR%\Config\Xcd\ConvertToXcd.js -q -i../../../Include;../..;/Library/Frameworks/QtCore.framework/Headers;/Library/Frameworks/QtGui.framework/Headers;/Library/Frameworks/QtNetwork.framework/Headers;/Library/Frameworks/QtScript.framework/Headers;/Library/Frameworks/QtXml.framework/Headers ..
---
+++ Impl/Xcd/ConvertIncludes.bat
@@ -0,0 +1,2 @@
+cd %IACFDIR%/Include
+%ACFDIR%\Config\Xcd\ConvertToXcd.js -q -i../..;/Library/Frameworks/QtCore.framework/Headers;/Library/Frameworks/QtGui.framework/Headers;/Library/Frameworks/QtNetwork.framework/Headers;/Library/Frameworks/QtScript.framework/Headers;/Library/Frameworks/QtXml.framework/Headers ..
