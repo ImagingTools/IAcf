458136195ff3745c276cfc576fbafffed24f8308
wg <wg@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2008-12-23 23:22:06 +0000

    Tutorial conversion script for XCode added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@74 f04c80de-fe56-0410-a3ee-ab8cdec974cc

---
+++ Impl/Xcd/ConvertTutorial.bat
@@ -0,0 +1,2 @@
+cd %IACFDIR%/Docs\Tutorial\Appl
+%ACFDIR%\Config\Xcd\ConvertToXcd.js -q -i../../../../../Include;../../../../../Impl;../..;/Library/Frameworks/QtCore.framework/Headers;/Library/Frameworks/QtGui.framework/Headers;/Library/Frameworks/QtNetwork.framework/Headers;/Library/Frameworks/QtScript.framework/Headers;/Library/Frameworks/QtXml.framework/Headers ..
