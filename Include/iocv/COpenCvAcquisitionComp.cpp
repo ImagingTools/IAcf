f1c5d8190972dfe8792cea5e5504ed4d06f2ebef
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2009-10-08 22:17:39 +0000

    Registries corrected. Work on OpenCV-based camera implementation continued.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@161 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Include/iocv/COpenCvAcquisitionComp.cpp
+++ Include/iocv/COpenCvAcquisitionComp.cpp
@@ -33,8 +33,8 @@ int COpenCvAcquisitionComp::DoProcessing(
 		return TS_INVALID;
 	}
 
-	iimg::IBitmap* outputBitmapPtr = dynamic_cast<iimg::IBitmap*>(outputPtr);
-	if (outputBitmapPtr == NULL){
+	istd::TChangeNotifier<iimg::IBitmap> bitmapPtr(dynamic_cast<iimg::IBitmap*>(outputPtr));
+	if (!bitmapPtr.IsValid()){
 		return TS_INVALID;
 	}
 
@@ -43,14 +43,39 @@ int COpenCvAcquisitionComp::DoProcessing(
 		return TS_INVALID;
 	}
 
-	if (outputBitmapPtr->CreateBitmap(
-					istd::CIndex2d(iplImagePtr->width, iplImagePtr->height),
-					iplImagePtr->imageData,
-					true,
-					iplImagePtr->widthStep,
-					iplImagePtr->depth,
-					iplImagePtr->nChannels)){
-		return TS_OK;
+
+	if (iplImagePtr->depth == IPL_DEPTH_8U){
+		switch (iplImagePtr->nChannels){
+			case 1:
+				if (!bitmapPtr->CreateBitmap(
+							istd::CIndex2d(iplImagePtr->width, iplImagePtr->height),
+							iplImagePtr->imageData,
+							true,
+							iplImagePtr->widthStep,
+							32,
+							4)){
+					return TS_INVALID;
+				}
+				break;
+			case 3:
+				if (bitmapPtr->CreateBitmap(istd::CIndex2d(iplImagePtr->width, iplImagePtr->height), 32, 4)){
+					for (int y = 0; y < iplImagePtr->height; y++){
+						I_BYTE* outputBitmapLinePtr = (I_BYTE*)bitmapPtr->GetLinePtr(y);
+						I_BYTE* inputBitmapLinePtr = (I_BYTE*)iplImagePtr->imageData + y * iplImagePtr->widthStep;
+
+						for (int x = 0; x < bitmapPtr->GetLineBytesCount(); x += 4){
+							int ix = (x >> 2) * 3;
+							outputBitmapLinePtr[x + 0] = inputBitmapLinePtr[ix + 2];
+							outputBitmapLinePtr[x + 1] = inputBitmapLinePtr[ix + 1];
+							outputBitmapLinePtr[x + 2] = inputBitmapLinePtr[ix + 0];
+							outputBitmapLinePtr[x + 3] = 255;
+						}
+					}
+
+					return TS_OK;
+				}
+				break;
+		}
 	}
 
 	return TS_INVALID;
@@ -61,8 +86,10 @@ int COpenCvAcquisitionComp::DoProcessing(
 
 istd::CIndex2d COpenCvAcquisitionComp::GetBitmapSize(const iprm::IParamsSet* /*paramsPtr*/) const
 {
+	int imageWidth = (int)cvGetCaptureProperty(m_cameraPtr, CV_CAP_PROP_FRAME_WIDTH);
+	int imageHeight = (int)cvGetCaptureProperty(m_cameraPtr, CV_CAP_PROP_FRAME_HEIGHT);
 
-	return istd::CIndex2d(-1, -1);	// unknown size
+	return istd::CIndex2d(imageWidth, imageHeight);
 }
 
 
@@ -86,7 +113,12 @@ void COpenCvAcquisitionComp::OnComponentCreated()
 {
 	BaseClass::OnComponentCreated();
 
-	m_cameraPtr = cvCreateCameraCapture(0);
+	m_cameraPtr = cvCreateCameraCapture(CV_CAP_ANY);
+
+	if (m_cameraPtr != NULL){
+		cvSetCaptureProperty(m_cameraPtr, CV_CAP_PROP_FRAME_WIDTH, 640);
+		cvSetCaptureProperty(m_cameraPtr, CV_CAP_PROP_FRAME_HEIGHT, 480);
+	}
 }
 
 
