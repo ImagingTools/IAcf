bb72211250ac2bff5afb316025cfbe8f3b9d1b36
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2012-10-18 11:42:32 +0000

    Build script supports parallel processing now.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@547 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Build/VC9/BuildAll.bat
+++ Build/VC9/BuildAll.bat
@@ -2,6 +2,6 @@ echo off
 
 call "%VS90COMNTOOLS%vsvars32.bat" x86
 
-msbuild IacfAll.sln
-msbuild IacfAll.sln /p:Configuration=Release
+msbuild IacfAll.sln /m
+msbuild IacfAll.sln /p:Configuration=Release /m
 pause
