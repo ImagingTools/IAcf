05f0814deb77eff3e5bd8fc167d59953e42af684
wg <wg@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2013-03-19 10:25:39 +0000

    Tutorial: Phonon player moved from ACF-Solutions.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@593 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Impl/PhononPck/PhononPck.cpp
+++ Impl/PhononPck/PhononPck.cpp
@@ -12,39 +12,14 @@ using namespace icomp;
 
 
 I_EXPORT_PACKAGE(
-			"AcfSln/Multimedia/Gui",
-			"Qt multimedia package",
+			"Iacf/Phonon",
+			"Phonon based package",
 			"Media" IM_PROJECT("ACF") IM_TAG("Qt Multimedia") IM_COMPANY("ImagingTools") IM_AUTHOR("\"Witold Gantzke\" \"Kirill Lepskiy\""));
-
-I_EXPORT_COMPONENT(
-			VideoPlayerGui,
-			"GUI used to create video player with parameters and control bar",
-			"Qt Video File Loader Play" IM_TAG("GUI"));
 
 I_EXPORT_COMPONENT(
 			PhononVideoViewGui,
 			"Video widget based on Phonon library",
 			"Phonon Widget Video Play" IM_TAG("GUI"));
-
-I_EXPORT_COMPONENT(
-			MediaLoader,
-			"Loader for multimedia files",
-			"Video File Loader Play" IM_TAG("Loader"));
-
-I_EXPORT_COMPONENT(
-			PlaybackControllerGui,
-			"Simple playback controller",
-			"Play Controller Frame Sequence Video File Loader Play" IM_TAG("GUI"));
-
-I_EXPORT_COMPONENT(
-			FrameSeqVideoController,
-			"Video controller playing images from directory on external bitmaps",
-			"Frame Sequence Bitmaps Images Video Medium Multimedia Directory Play" IM_TAG("Image"));
-
-I_EXPORT_COMPONENT(
-			SvgBitmapLoader,
-			"Loader for vector graphics SVG files rendering it to bitmap",
-			"Bitmap Vector Renderer SVG QT Scalable Graphics" IM_TAG("Image File Loader"));
 
 
 } // namespace QtMmPck
