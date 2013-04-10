66b7129b6e14a28ab7bae9f9848a07461ed9be46
wg <wg@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2013-04-10 06:52:22 +0000

    Copy scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@599 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Build/VC9/CopyDllToBin_Debug.bat
+++ Build/VC9/CopyDllToBin_Debug.bat
@@ -1,2 +1,5 @@
-copy /Y %FFMPEGDIR_20121010%\bin\*.dll %ACFDIR%\Bin\DebugVC9
-copy /Y %OPENCVDIR%\Bin\*.dll %ACFDIR%\Bin\DebugVC9
+copy /Y %AVTDIR%\FireGrab\*.dll  %ACFSLNDIR%\Bin\DebugVC9
+copy /Y %MILDIR%\Dll\*.dll  %ACFSLNDIR%\Bin\DebugVC9
+copy /Y %OPENCVDIR%\Bin\*.dll  %ACFSLNDIR%\Bin\DebugVC9
+copy /Y %FFMPEGDIR_20121010%\bin\*.dll %ACFSLNDIR%\Bin\DebugVC9
+copy /Y %OPENCVDIR%\Bin\*.dll %ACFSLNDIR%\Bin\DebugVC9
--- Build/VC9/CopyDllToBin_Release.bat
+++ Build/VC9/CopyDllToBin_Release.bat
@@ -1,2 +1,5 @@
-copy /Y %FFMPEGDIR_20121010%\bin\*.dll %ACFDIR%\Bin\ReleaseVC9
-copy /Y %OPENCVDIR%\Bin\*.dll %ACFDIR%\Bin\ReleaseVC9
+copy /Y %AVTDIR%\FireGrab\*.dll  %ACFSLNDIR%\Bin\ReleaseVC9
+copy /Y %MILDIR%\Dll\*.dll  %ACFSLNDIR%\Bin\ReleaseVC9
+copy /Y %OPENCVDIR%\Bin\*.dll  %ACFSLNDIR%\Bin\ReleaseVC9
+copy /Y %FFMPEGDIR_20121010%\bin\*.dll %ACFSLNDIR%\Bin\ReleaseVC9
+copy /Y %OPENCVDIR%\Bin\*.dll %ACFSLNDIR%\Bin\ReleaseVC9
