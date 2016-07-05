ab9d5f4ca69e8754302a3b8acebecd6c401d306d
wg <wg@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2016-07-05 15:34:56 +0000

    VC10 generator corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@739 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Build/VC10/_CreateVCProjectsFromQMake.bat
+++ Build/VC10/_CreateVCProjectsFromQMake.bat
@@ -2,15 +2,17 @@ rem echo off
 
 set COMPILER_EXT=VC10
 set QMAKESPEC=%QTDIR%/mkspecs/win32-msvc2010
+set path=%path%;%QTDIR%\bin
 
-cd ..\..
+cd %~dp0\..\..
 
 echo Generating %COMPILER_EXT% projects...
 cd Build/QMake
-qmake -recursive -tp vc IacfAll.pro
-cd ../..
+%QTDIR%\bin\qmake -recursive -tp vc
+cd %~dp0\..\..
 
-call %ACFDIR%/Config/QMake/CopyVCProjToSubdir.js %COMPILER_EXT% -replace%QTDIR%=$(QTDIR) -replace%ACFDIR%\=$(ACFDIR)\ -replace%ACFSLNDIR%\=$(ACFSLNDIR)\ -replace%CD%\=$(RelativePath)\
+call %ACFDIR%\Config\QMake\CopyVCProjToSubdir.js %COMPILER_EXT% -replace%QTDIR%=$(QTDIR) -replace%ACFDIR%\=$(ACFDIR)\ -replace%ACFSLNDIR%\=$(ACFSLNDIR)\ -replace%ACF_TOOLS_BIN%\=$(ACF_TOOLS_BIN)\ -replace%CD%\=$(RelativePath)\
 
 
-cd Build\VC10
+cd %~dp0\
+
