2429e634dab16df47b8ca4205d248e7a11300b4a
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2017-05-23 12:53:49 +0000

    Masking implemented in the blob processor
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@780 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Include/iocv/COcvBlobProcessorComp.cpp
+++ Include/iocv/COcvBlobProcessorComp.cpp
@@ -12,6 +12,8 @@
 #include <iimg/CScanlineMask.h>
 #include <iprm/TParamsPtr.h>
 #include <i2d/CPolygon.h>
+#include <ilog/CExtMessage.h>
+#include <iimg/CBitmap.h>
 
 // ACF-Solutions includes
 #include <iblob/CBlobFeature.h>
@@ -38,32 +40,75 @@ bool COcvBlobProcessorComp::CalculateBlobs(
 
 	ibase::CSize size = image.GetImageSize();
 
-	// create image mask
-	i2d::CRect clipArea(size);
-	iimg::CScanlineMask mask;
+	// Initialize input bitmap:
+	cv::Mat tmpBinaryImage;
+
+	// Filter out masked points:
 	if (aoiPtr != NULL){
+		iimg::CScanlineMask mask;
 		mask.SetCalibration(image.GetCalibration());
 
+		i2d::CRect clipArea(size);
 		if (!mask.CreateFromGeometry(*aoiPtr, &clipArea)){
 			SendErrorMessage(0, QObject::tr("AOI type is not supported"));
 
 			return false;
 		}
+
+		mask.Invert(clipArea);
+
+		iimg::CBitmap maskedBitmap;
+		maskedBitmap.CopyFrom(image);
+
+		istd::CIntRange lineRange(0, size.GetX());
+
+		for (int y = 0; y < size.GetY(); ++y){
+			const istd::CIntRanges* outputRangesPtr = mask.GetPixelRanges(y);
+			if (outputRangesPtr != NULL){
+				quint8* inputLinePtr = static_cast<quint8*>(maskedBitmap.GetLinePtr(y));
+
+				istd::CIntRanges::RangeList rangeList;
+				outputRangesPtr->GetAsList(lineRange, rangeList);
+				for (istd::CIntRanges::RangeList::ConstIterator iter = rangeList.constBegin();
+					iter != rangeList.constEnd();
+					++iter){
+					const istd::CIntRange& rangeH = *iter;
+					Q_ASSERT(rangeH.GetMinValue() >= 0);
+					Q_ASSERT(rangeH.GetMaxValue() <= size.GetX());
+
+					for (int x = rangeH.GetMinValue(); x < rangeH.GetMaxValue(); ++x){
+						inputLinePtr[x] = 0;
+					}
+				}
+			}
+		}
+
+		void* imageDataBufferPtr = const_cast<void*>(maskedBitmap.GetLinePtr(0));
+		cv::Mat inputBitmap(size.GetY(), size.GetX(), CV_8UC1, imageDataBufferPtr, maskedBitmap.GetLineBytesCount());
+		tmpBinaryImage = inputBitmap.clone();
 	}
 	else{
-		mask.CreateFilled(clipArea);
+		void* imageDataBufferPtr = const_cast<void*>(image.GetLinePtr(0));
+		cv::Mat inputBitmap(size.GetY(), size.GetX(), CV_8UC1, imageDataBufferPtr, image.GetLineBytesCount());
+		tmpBinaryImage = inputBitmap.clone();
 	}
 
-	// Initialize input bitmap:
-	void* imageDataBufferPtr = const_cast<void*>(image.GetLinePtr(0));
-	cv::Mat inputBitmap(image.GetImageSize().GetY(), image.GetImageSize().GetX(), CV_8UC1, imageDataBufferPtr, image .GetLineBytesCount());
-
 	// Get contours from the binary image:
-	cv::Mat tmpBinaryImage = inputBitmap.clone();
 	std::vector<std::vector<cv::Point> > contours;
 	findContours(tmpBinaryImage, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 
 	// Get found blobs:
+	int blobsCount = 0;
+
+	istd::TDelPtr<ilog::CExtMessage> blobMessagePtr;
+	if (m_resultConsumerCompPtr.IsValid()){
+		blobMessagePtr.SetPtr(new ilog::CExtMessage(
+			istd::IInformationProvider::IC_INFO,
+			MI_FOUND_BLOB,
+			"",
+			"PreciseBlobProcessor"));
+	}
+
 	for (int contourIndex = 0; contourIndex < int(contours.size()); contourIndex++){
 		cv::Moments moms = cv::moments(cv::Mat(contours[contourIndex]));
 
@@ -87,17 +132,27 @@ bool COcvBlobProcessorComp::CalculateBlobs(
 			passedByFilter = IsBlobAcceptedByFilter(*filterParamsPtr, area, perimeter, circularity);
 		}
 
-		bool passedByMask = true;
+		if (passedByFilter){
+			blobsCount++;
 
-		if (aoiPtr != NULL){
-			passedByMask = aoiPtr->GetBoundingBox().Contains(istd::CIndex2d(location.x, location.y));
-		}
+			i2d::CVector2d position = i2d::CVector2d(location.x, location.y);
 
-		if (passedByFilter && passedByMask){
-			iblob::CBlobFeature* blobFeaturePtr = new iblob::CBlobFeature(area, perimeter, i2d::CVector2d(location.x, location.y));
-
+			iblob::CBlobFeature* blobFeaturePtr = new iblob::CBlobFeature(area, perimeter, position);
 			result.AddFeature(blobFeaturePtr);
+
+			if (blobMessagePtr.IsValid()){
+				i2d::CCircle* blobMessageCirclePtr = new imod::TModelWrap<i2d::CCircle>();
+				blobMessageCirclePtr->SetPosition(position);
+				blobMessageCirclePtr->SetRadius(qSqrt(area / I_PI));
+				blobMessagePtr->InsertAttachedObject(blobMessageCirclePtr, QObject::tr("Blob %1, Pos.: (%2; %3), Area: %4").arg(blobsCount).arg(position.GetX()).arg(position.GetY()).arg(area));
+			}
 		}
+	}
+
+	if (blobMessagePtr.IsValid()){
+		blobMessagePtr->SetText(QObject::tr("Found %1 blob(s)").arg(blobsCount));
+
+		m_resultConsumerCompPtr->AddMessage(ilog::IMessageConsumer::MessagePtr(blobMessagePtr.PopPtr()));
 	}
 
 	return true;
--- Include/iocv/COcvBlobProcessorComp.h
+++ Include/iocv/COcvBlobProcessorComp.h
@@ -15,7 +15,14 @@ public:
 	typedef iblob::CBlobProcessorCompBase BaseClass;
 
 	I_BEGIN_COMPONENT(COcvBlobProcessorComp);
+	I_ASSIGN(m_resultConsumerCompPtr, "ResultConsumer", "Consumer of result messages with geometrical layout", false, "ResultConsumer");
+	I_ASSIGN(m_tempConsumerCompPtr, "TempConsumer", "Consumer of temporary result messages with geometrical layout", false, "TempConsumer");
 	I_END_COMPONENT;
+
+	enum MessageId
+	{
+		MI_FOUND_BLOB = 0x9354f0
+	};
 
 protected:
 	// reimplemented (iblob::CBlobProcessorCompBase)
@@ -25,6 +32,10 @@ protected:
 				const i2d::IObject2d* aoiPtr,
 				const iimg::IBitmap& image,
 				iipr::IFeaturesConsumer& result);
+
+private:
+	I_REF(ilog::IMessageConsumer, m_resultConsumerCompPtr);
+	I_REF(ilog::IMessageConsumer, m_tempConsumerCompPtr);
 };
 
 
