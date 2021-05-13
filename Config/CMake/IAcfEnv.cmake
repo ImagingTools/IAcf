2dd03f117fc3d8552012c3528b7c61aa1d1e127b
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2021-05-13 11:29:07 +0000

    CMake corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@875 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Config/CMake/IAcfEnv.cmake
+++ Config/CMake/IAcfEnv.cmake
@@ -12,9 +12,16 @@ if(NOT DEFINED IACFDIR)
 	set(IACFDIR "$ENV{IACFDIR}")
 endif()
 
+
+if(NOT DEFINED OPENCVDIR_3_3_1)
+	set(OPENCVDIR_3_3_1 "${ACFDIR}/../3rdParty/OpenCV/3.3.1")
+endif()
+
 include_directories("${IACFDIR}/AuxInclude/Qt${QT_DEFAULT_MAJOR_VERSION}_${TARGETNAME}")
 include_directories("${IACFDIR}/Include")
 include_directories("${IACFDIR}/Impl")
+
+link_directories(${IACFDIR}/Lib/${CMAKE_BUILD_TYPE}${TARGETNAME})
 
 
 
