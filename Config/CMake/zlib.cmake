efa4d787c4e521234f51f3f3297c8a78bbbcc800
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2021-05-13 11:30:58 +0000

    CMake corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@876 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Config/CMake/zlib.cmake
+++ Config/CMake/zlib.cmake
@@ -1,15 +1,3 @@
- if(DEFINED ENV{ZLIBDIR})
-		set(ZLIBDIR $ENV{ZLIBDIR})
-	else()
-		set(ZLIBDIR "${ACFDIR}/../3rdParty/quazip")
-	endif()
-
 include_directories(${ZLIBDIR}/include)
-#find_library (zlibPath 
-#				NAMES zlib.lib
-#				PATHS "C:/Work/Rpos/AcfPublic/3rdParty/zlib/lib/DebugVC15_64/"
-#				NO_DEFAULT_PATH)
-#				#Path ${ZLIBDIR}/lib/${CMAKE_BUILD_TYPE}${TARGETNAME})
-#				message("zlibPath: " ${zlibPath})
 
 target_link_libraries(${PROJECT_NAME} ${ZLIBDIR}/lib/${CMAKE_BUILD_TYPE}${TARGETNAME}/zlib.lib)
