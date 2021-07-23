edba165a6ebf23e48e7b4ec35f6afe50c55ca97b
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2021-07-23 13:58:52 +0000

    CMake projects improved
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@878 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Config/CMake/zlib.cmake
+++ Config/CMake/zlib.cmake
@@ -1,3 +1,10 @@
 include_directories(${ZLIBDIR}/include)
 
-target_link_libraries(${PROJECT_NAME} ${ZLIBDIR}/lib/${CMAKE_BUILD_TYPE}${TARGETNAME}/zlib.lib)
+if(WIN32)
+	target_link_libraries(${PROJECT_NAME} ${ZLIBDIR}/lib/${CMAKE_BUILD_TYPE}${TARGETNAME}/zlib.lib)
+endif()
+
+if(UNIX)
+	target_link_libraries(${PROJECT_NAME} -lz)
+endif()
+
