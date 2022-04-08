af5a2797644921f034b295f722eca8c6f4cee192
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2022-04-08 14:16:39 +0000

    CMake fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@887 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Config/CMake/OpenCV_4_5_3.cmake
+++ Config/CMake/OpenCV_4_5_3.cmake
@@ -1,7 +1,7 @@
 include_directories("$ENV{OPENCVDIR_4_5_3}/include")
 
 if(WIN32)
-	if(CMAKE_BUILD_TYPE EQUAL "Relase")
+        if(CMAKE_BUILD_TYPE EQUAL "Release")
         target_link_libraries(${PROJECT_NAME} opencv_world453)
     else()
         target_link_libraries(${PROJECT_NAME} opencv_world453d)
