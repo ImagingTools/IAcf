09846de40dd1948c004b52d60d2b9f45ec5a6ea6
kl <kl@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2023-08-10 12:25:31 +0000

    Warning fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@903 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Include/iocv/COcvDenoisingProcessorComp.cpp
+++ Include/iocv/COcvDenoisingProcessorComp.cpp
@@ -23,7 +23,7 @@ namespace iocv
 // reimplemented (iproc::IProcessor)
 
 int COcvDenoisingProcessorComp::DoProcessing(
-			const iprm::IParamsSet* paramsPtr,
+			const iprm::IParamsSet* /*paramsPtr*/,
 			const istd::IPolymorphic* inputPtr,
 			istd::IChangeable* outputPtr,
 			ibase::IProgressManager* /*progressManagerPtr*/)
