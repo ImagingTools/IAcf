4d84f7bae29e6dae3bb5abfaf33438692f3a799f
Roman.Kucherenko <Roman.Kucherenko@f04c80de-fe56-0410-a3ee-ab8cdec974cc>
2025-03-07 04:32:25 +0000

    CMake formatting corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/IAcf@920 f04c80de-fe56-0410-a3ee-ab8cdec974cc

--- Config/CMake/OpenCV_4_5_3.cmake
+++ Config/CMake/OpenCV_4_5_3.cmake
@@ -1,13 +1,13 @@
 include_directories("$ENV{OPENCVDIR_4_5_3}/include")
 
 if(WIN32)
-    if(CMAKE_BUILD_TYPE STREQUAL "Release")
-        target_link_libraries(${PROJECT_NAME} $ENV{OPENCVDIR_4_5_3}/lib/${CMAKE_BUILD_TYPE}_${COMPILER_CODE}/opencv_world453.lib)
-    else()
+	if(CMAKE_BUILD_TYPE STREQUAL "Release")
+		target_link_libraries(${PROJECT_NAME} $ENV{OPENCVDIR_4_5_3}/lib/${CMAKE_BUILD_TYPE}_${COMPILER_CODE}/opencv_world453.lib)
+	else()
 		target_link_libraries(${PROJECT_NAME} $ENV{OPENCVDIR_4_5_3}/lib/${CMAKE_BUILD_TYPE}_${COMPILER_CODE}/opencv_world453d.lib)
-    endif()
+	endif()
 endif()
 
 if(UNIX)
-    target_link_libraries(${PROJECT_NAME} opencv_world453)
+	target_link_libraries(${PROJECT_NAME} opencv_world453)
 endif()
